fca6246449dcc05f42803cd12d2172cdf6e44bf7
halkity <saito75061@gmail.com>
2021-01-13 11:46:22 +0900

    マイクロポスト機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,14 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  root   'static_pages#home'                    # トップページ
-  get    '/help',    to: 'static_pages#help'    # ヘルプページ
-  get    '/about',   to: 'static_pages#about'   # アバウトページ
-  get    '/contact', to: 'static_pages#contact' # 
-  get    '/signup',  to: 'users#new'            # 新規登録ページ
-  get    '/login',   to: 'sessions#new'         # ログインページ
-  post   '/login',   to: 'sessions#create'      # ログイン処理
-  delete '/logout',  to: 'sessions#destroy'     # ログアウト処理
-  resources :users  # 新規登録、詳細、編集、削除
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
-  # GET     /users          users#index   一覧
-  # POST    /users          users#create  新規登録処理
-  # GET     /users/new      users#new     新規登録ページ
-  # GET     /users/id/edit users#edit     
-  # GET     /users/id      users#show     プロフィール
-  # PATCH   /users/id      users#update   
-  # PUT     /users/id      users#update   
-  # DELETE  /users/id      users#destroy  
+  resources :microposts,          only: [:create, :destroy]
 end
